100fb1065f8ada3aa80d3dd9c8f3f33ab5b25ad4
luongyen <luongyen1508@gmail.com>
2019-05-22 14:48:19 +0700

    first commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+use Auth;
+use App\User;
+use Tymon\JWTAuth\JWTAuth;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+
+class LoginController extends Controller
+{
+    use ApiResponser;
+
+    protected $jwt;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(JWTAuth $jwt)
+    {
+        $this->jwt = $jwt;
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required',
+        ]);
+        $user = User::where(['email' => $request->email])->first();
+        if(isset($user)){
+                $data['token'] = $this->jwt->attempt($request->only('email', 'password'));
+                if ($data['token'] == false) {
+                    return $this->successResponseMessage($data, 413, "Password inccorect");
+                } else {
+                    $data['user'] = new UserResource($user);
+                    return $this->successResponseMessage($data, 200, "Login success");
+                }           
+        }else{
+            $data['token'] = false;
+            return $this->successResponseMessage($data, 413, "Email inccorect");
+        }
+
+    }
+
+    public function logout(Request $request){
+        Auth::logout();
+        return $this->successResponseMessage(new \stdClass(), 200, "Logout success");
+    }
+}
---
+++ app/User.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+
+class User extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
+{
+    use Authenticatable, Authorizable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email','password','avartar','role'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    public static function userCreate($data)
+    {
+        $user = User::create([
+            'name' => $data->name,
+            'email' => $data->email,
+            'password' => Hash::make($data->password),
+            'avartar' => env('APP_URL').'/avatars/default.jpg',
+            "role"=>0
+        ]);
+        return $user;
+
+    }
+}
